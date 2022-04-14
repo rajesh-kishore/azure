959ea40339b81ff12e73cf384a78486f5edc79cb
Rajesh Kishore <rajesh.kishore@sap.com>
2022-04-14 14:26:00 +0530

    made it as web application

--- demo/src/main/java/com/kishore/azure/demo/DemoApplication.java
+++ demo/src/main/java/com/kishore/azure/demo/DemoApplication.java
@@ -2,9 +2,25 @@ package com.kishore.azure.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.boot.*;
+import org.springframework.boot.autoconfigure.*;
+import org.springframework.stereotype.*;
+import org.springframework.web.bind.annotation.*;
 
 @SpringBootApplication
+@Controller
+@EnableAutoConfiguration
 public class DemoApplication {
+
+
+       @RequestMapping("/")
+       @ResponseBody
+       String home() {
+          return "Welcome to Azure";
+       }
 
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
